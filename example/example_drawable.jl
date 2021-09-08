c478071e2173bddbcefbf407e3faf6a74935bf08
Siddharth Bhatia <sidb60200@gmail.com>
2021-09-08 12:59:33 +0530

    add simple example

---
+++ example/example_drawable.jl
@@ -0,0 +1,38 @@
+import SimpleDraw as SD
+import SimplePaint as SP
+
+#####
+##### example drawable
+#####
+
+mutable struct ExampleDrawable{I, C}
+    background::SD.Background{C}
+    hollow_cross::SD.HollowCross{I, C}
+    line::SD.Line{I, C}
+end
+
+function SD.draw!(image::AbstractMatrix, drawable::ExampleDrawable)
+    SD.draw!(image, drawable.background)
+    SD.draw!(image, drawable.hollow_cross)
+    SD.draw!(image, drawable.line)
+    return nothing
+end
+
+#####
+##### driver code
+#####
+
+height_image = 512
+width_image = 512
+image = zeros(UInt32, height_image, width_image)
+
+background = SD.Background(0x00FFFFFF)
+cross = SD.HollowCross(20, 30, 16, 0x00000000)
+line = SD.Line(height_image ÷ 4, width_image ÷ 8, height_image - height_image ÷ 4 + 1, width_image - width_image ÷ 8 + 1, 0x00FF0000)
+drawable = ExampleDrawable(background, cross, line)
+
+path = Int[]
+
+drawing_context = SP.DrawingContext(image, drawable, path)
+
+SP.start_session!(drawing_context)
